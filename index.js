b9304bd7e7ad3646721fd2219ce78777eef25234
nagaveda <gajjalanagaveda@karunya.edu.in>
2020-06-13 19:27:42 +0530

    mongoose part-3

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const Dishes = require('./models/dishes');
 
+
 const url = 'mongodb://localhost:27017/conFusion';
 
 const connect = mongoose.connect(url);
@@ -16,11 +17,24 @@ connect.then((db) => {
     .then((dish) => {
         console.log(dish);
 
-        return Dishes.find({}).exec();
+        return Dishes.findByIdAndUpdate(dish._id, {
+            $set: { description: 'Updated test'}
+        },{
+            new: true
+        }).exec();
     })
-    .then((dishes) => {
-        console.log(dishes);
+    .then((dish) => {
+        console.log(dish);
+        dish.comments.push({
+            rating: 5,
+            comment: 'I\'m getting a sinking feeling!',
+            author: 'Leonardo di Carpaccio'
+        });
 
+        return dish.save();
+    })
+    .then((dish) => {
+        console.log(dish);
         return Dishes.remove({});
     })
     .then(() => {
